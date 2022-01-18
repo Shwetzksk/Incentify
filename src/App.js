6799b43591a66e725ed5c6d5b432686544f06157
Mihir Mulay <55177881+MihirMulay2001@users.noreply.github.com>
2022-01-19 01:59:23 +0530

    added basix animation

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
       <Landing />
       <Purpose />
       <Clubs />
+      <Purpose />
     </div>
   );
 }
--- src/sections/clubs/Clubs.jsx
+++ src/sections/clubs/Clubs.jsx
@@ -1,10 +1,116 @@
 import React from 'react'
 import styles from './_clubs.module.scss'
+import PhoneImage from '../../assets/icons/phone-straight.png'
+import { useInView } from 'react-intersection-observer'
+import { motion, useAnimation } from 'framer-motion'
 
-export default function Clubs() {
-    return (
-        <div className={styles.section}>
-            <h1>Introducing Clubs from Incentify 🕺</h1>
-        </div>
+
+const TextBox = (props) => {
+    const {top, right, bottom, left, inView} = props
+    const controls = useAnimation()
+
+    React.useEffect(()=>{
+        if(!inView)
+        {
+            controls.start({
+                opacity: 0,
+                y: 200,
+                transition: {duration: 0}
+            })
+        }
+        else if(inView)
+        {
+            controls.start({
+                opacity: 1,
+                y: 0,
+                transition: {duration: 1, delay: props.delay/2}
+            })
+        }
+        
+    },[inView])
+
+    return(
+        <motion.div animate={controls} className={styles.textbox} style={{top, right, bottom, left}}>
+            {props.content}
+        </motion.div>
     )
 }
+
+export default function Clubs() {
+    const {ref, inView} = useInView({
+        threshold: 0.3
+    })
+    console.log(inView);
+    return (
+        <>
+            <div className={styles.section}>
+                <h1>Introducing Clubs from Incentify 🕺</h1>
+                <motion.div ref={ref} className={styles.phone}>
+                    <img src={PhoneImage} alt="phone" />
+                    <TextBox 
+                    top="-5%" 
+                    left="-140%"
+                    inView={inView}
+                    delay={0}
+                    content="⚡ A playlist curated by users with similar music taste & vibes" />
+                <TextBox 
+                    top="20%"
+                    right="-145%"
+                    inView={inView}
+                    delay={1}
+                    content="🎵 Share songs with friends in your Club, no matter which music app they use" />
+                <TextBox 
+                    top="60%"
+                    left="-145%"
+                    delay={2}
+                    inView={inView}
+                    content="💎 Discover hidden gems from Clubs on Incentify" />
+                <TextBox 
+                    top="90%"
+                    left="-20%"
+                    delay={3}
+                    inView={inView}
+                    content="Think you’ve got great taste? Start a Club now 🔥" />
+                </motion.div>
+                 
+            </div>
+        </>
+        
+    )
+}
+
+
+/*
+
+<div className={styles.section}>
+                <h1>Introducing Clubs from Incentify 🕺</h1>
+                <div ref={ref} className={styles.phone}>
+                    <img src={PhoneImage} alt="phone" />
+                </div>
+            </div>
+            <div className={styles.textcont}>
+                <TextBox 
+                top="15%"
+                left="11%"
+                content="⚡ A playlist curated by users with similar music taste & vibes" />
+            </div>
+            <div className={styles.textcont}>
+                <TextBox 
+                top="25%"
+                left="30%"
+                content="🎵 Share songs with friends in your Club, no matter which music app they use" />
+            </div>
+            <div className={styles.textcont}>
+                <TextBox 
+                top="20%"
+                left="100%"
+                content="💎 Discover hidden gems from Clubs on Incentify" />
+            </div>
+            <div className={styles.textcont}>
+                <TextBox 
+                top="60%"
+                left="10%"
+                content="Think you’ve got great taste? Start a Club now 🔥" />
+            </div>
+
+            */
